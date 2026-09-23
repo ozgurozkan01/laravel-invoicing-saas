b40187b6b63ee0cab11aaff02f167d7369ed62a8
Özgür ÖZKAN <230201121@kocaeli.edu.tr>
2026-09-23 20:49:28 +0300

    fix broken migration progress

--- database/migrations/2026_09_22_192523_create_clients_table.php
+++ database/migrations/2026_09_22_192523_create_clients_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
             $table->string('name');
             $table->string('email');
             $table->string('phone');
