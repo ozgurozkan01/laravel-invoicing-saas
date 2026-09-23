8ee294e27ed703e2b63b1bf68a2e02f3e56b02ba
Özgür ÖZKAN <230201121@kocaeli.edu.tr>
2026-09-23 22:44:03 +0300

    add auto-assign Free plan on user registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -36,10 +37,13 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $freePlan = Plan::where('name', 'Free')->firstOrFail();
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'plan_id' => $freePlan->id
         ]);
 
         event(new Registered($user));
