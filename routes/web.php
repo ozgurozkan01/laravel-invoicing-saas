4921baa5b8941e3fce8fb5d8ffce7a21ac813497
Özgür ÖZKAN <230201121@kocaeli.edu.tr>
2026-09-25 21:17:15 +0300

    add ClientController methods via resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,10 +12,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('features', function () { return view('features'); })->name('features.index');
+Route::get('pricing', function () { return view('pricing'); })->name('pricing.index');
+Route::get('faq', function () { return view('faq'); })->name('faq.index');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('clients', ClientController::class)->except(['show']);
 
     // Route::post('/invoices', [InvoiceController::class, 'store'])->middleware(['invoice.limit']);
 });
